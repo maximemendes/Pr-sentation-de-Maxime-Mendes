a467e84e51d987ba126ff8a4ca13ff97a0b7db73
Maxime <michaelcosta37550@gmail.com>
2019-01-19 11:07:15 +0100

    Présentation CEFIM

---
+++ index.php
@@ -0,0 +1,276 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Présentation de Maxime MENDES</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+        <article class="markdown-body">
+
+            <!-- Introduction -->
+            <h1>Maxime MENDES</h1>
+            <p>Vous retrouvez à travers ce site, une présentation de mon profil, de mes études, de mes centres d'intérêts et de mes compétences. Pour cette présentation, j'ai décidé d'utiliser un style <strong>Markdown</strong> que j'apprécie pour sa simplicité et son design épuré.</p>
+
+            <p>Vous êtes prêt ? <strong>Alors allons-y !</strong></p>
+
+            <!-- Présentation -->
+            <h2><img src="assets/images/presentation.png" style="height:25px;" alt="Présentation" title="Présentation"> &bull; Quelques mots pour commencer...</h2>
+            <p>Comme vous l'avez compris, je m'appelle <strong>Maxime MENDES</strong>. Âgé de 18 ans, j'étudie actuellement à l'<strong><a href="https://www.univ-tours.fr" target="_blank">Université de Tours</a></strong> en <i>Première année de Licence de Droit</i>. Cependant, ce parcours scolaire ne reflète pas mes ambitions pour mon avenir professionnel. En effet, derrière un étudiant moyen dans le domaine du Droit, se cache... un passionné de développement web et d'informatique !</p>
+
+            <p>Depuis 2012, je me forme par le biais de livres et de cours en ligne au développement de sites internets tout en m'entraînant sur des sites hébergés en local ou des projets associatifs et bénévoles. Je cherche aujourd'hui à entrer dans une formation certifiante afin d'approfondir au maximum mes compétences afin d'être au plus haut de mes capacités et de pouvoir intégrer rapidement le marché de l'emploi.</p>
+
+            <center><a href="assets/images/maxime_mendes.jpg" target="_blank"><img style="height:95px;" src="assets/images/photo_maxime.png"></a></center>
+
+            <!-- Parcours scolaire -->
+            <h2><img src="assets/images/parcours.png" style="height:25px;" alt="Parcours scolaire" title="Parcours scolaire"> &bull; Parcours scolaire</h2>
+            <ul>
+                <li><strong>Collège Jean Moulin (SANNOIS 95110)</strong></li>
+                2012/2013 - 2014/2015<br /> 
+                Scolarité de la 6<sup>ème</sup> à la 3<sup>ème</sup>.
+                <p></p>
+
+                <li><strong>Lycée Grandmont (TOURS - 37000)</strong></li>
+                2015/2016 - 2017/2018<br />
+                Scolarité de la Seconde à la Terminale.
+                <p></p>
+
+                <li><strong>Université de Tours - Faculté de Droit (TOURS - 37000)</strong></li>
+                2018/2019<br />
+                Première année de Licence de Droit. 
+                <p></p>
+            </ul>
+
+            <!-- Diplômes et certifications -->
+            <h2><img src="assets/images/diplomes.png" style="height:25px;" alt="Dîplomes et certifications" title="Dîplomes et certifications"> &bull; Diplômes et certifications</h2>
+            <ul>
+                <li><strong>Diplôme National du Brevet</strong></li>
+                Obtenu en 2015<br />
+                <p></p>
+
+                <li><strong>Baccalauréat Sciences et Technologies du Management et de la Gestion - Option Marketing</strong></li>
+                Obtenu en 2018<br />
+                <i>Mention Assez Bien</i><br />
+                <p></p>
+
+                <li><strong>Certification Google Ateliers Numériques (IAB) - Fondamentaux du Marketing Digital</strong></li>
+                Obtenu en 2018<br />
+                <a href="assets/images/certification.png" target="_blank">Voir la certification</a>
+                <p></p>
+            </ul>
+
+            <!-- Expériences et projets -->
+            <h2><img src="assets/images/experiences.png" style="height:25px;" alt="Expériences et projets" title="Expériences et projets"> &bull; Mes expériences et projets</h2>
+            <ul>
+                <li><strong>Diverses missions - Association &laquo; <i>Vivre Avec Leur Diabète 95</i> &raquo;</strong>
+
+                    <ol><strong>Site internet</strong></ol>
+                    J'ai bénévolement crée un site internet (HTML - CSS - Javascript - SQL) pour une association se nommant &laquo; Vivre Avec Leur Diabète 95 &raquo; étant situé dans le Val-d'Oise. Le site internet était basé sur une template responsive et avait pour objectif de rescencer les événements de l'association, de partager des articles concernants le Diabète de type 1 gérés par une base de données MySQL et de trouver différents contacts dans le département afin de pouvoir discuter du diabète.
+
+                    <ol><strong>Réseaux sociaux</strong></ol>
+                    J'ai également crée différents visuels (logos, bannières, affiches) et mis en place une page Facebook avec des publicités ciblées visant à promouvoir l'association et l'a faire connaître dans le département du Val-d'Oise.
+
+                    <ol><strong>Logiciel de gestion des membres</strong></ol>
+                    Création en C# d'un logiciel de gestion des membres avec une base de données interne. Celui-ci disposait d'un panel de connexion à identifiant unique.
+
+                </li>
+                <p></p>
+                <!-- Plugin Facebook - Vivre avec Leur Diabète -->
+                <center>
+                    <div class="fb-page" data-href="https://www.facebook.com/VivreAvecLeurDiabete/" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/VivreAvecLeurDiabete/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/VivreAvecLeurDiabete/">Vivre Avec Leur Diabète</a></blockquote></div>
+                </center>
+                <!-- Plugin Facebook - Vivre avec Leur Diabète -->
+                <p></p>
+                <hr>
+                <li><strong>Projet - &laquo; ToursDélices &raquo;</strong>
+
+                    <ol><strong>Site internet</strong></ol>
+                    Création d'un site internet (HTML - CSS - PHP - SQL - Javascript - Framework Bootstrap) permettant de visualiser (client) diverses listes de produits surgelés (beignets salés, beignets sucrés, gâteaux, ...) et de gérer (administrateur) la liste des produits, de pouvoir commander (client) et de gérer les commandes (administrateur), de pouvoir intégralement gérer les stocks.
+
+                    <ol><strong>Visuels</strong></ol>
+                    Création d'une identité graphique afin de promouvoir la société (Logos, affiches, stickers à coller sur les véhicules utilitaires, ...).
+
+                    <center><img src="assets/images/logo-toursdelices.png" style="height: 120px;" alt="Logo de ToursDélices" title="Logo de ToursDélices"></center>
+                </li>
+                <p></p>
+                <hr>
+                <li><strong>Bénévolat - Ventes de châteaux - Notaire</strong>
+
+                    <ol><strong>Site internet</strong></ol>
+                    Création d'une page de renvoi incluant deux boutons. Le premier bouton redirige vers l'adaptation d'un site existant et appartenant à un Château. L'adaptation a été effectué pour promouvoir la vente de celui-ci par un procédé de vente aux enchères en ligne réalisé par les Notaires. Le second bouton redirige vers un site basé sur le framework Bootstrap adapté pour la vente d'un Manoir par le procédé de vente aux enchères en ligne réalisé par les Notaires. L'ensemble des sites incluaient une page dédiée aux mentions légales, un plugin Google Map avec la localisation de l'&Eacute;tude Notariale et des châteaux en ventes ainsi qu'une page de contact.
+
+                    <ol><strong>Quelques captures d'écran</strong></ol>
+                    <a href="assets/images/apercu2_index.png" target="_blank">Aperçu - Page de renvoi</a> &bull; <a href="assets/images/apercu3_index2.png" target="_blank">Aperçu - Page de renvoi (2)</a> &bull; <a href="assets/images/apercu1_restigne.png" target="_blank">Aperçu - Site du manoir</a><br>
+                    <small>Certaines informations ont dûes être modifiées et floutées afin de respecter les obligations contractuelles.</small>
+
+                </li>
+                <p></p>
+                <hr>
+                <li><strong>Stage - Mairie de Sannois - Service Informatique</strong>
+
+                    <ol><strong>Missions techniques</strong></ol>
+                    Installation de plusieurs ordinateurs dans un établissement scolaire, installation de matériel informatique au sein d'une crèche municipale et vérification de divers systèmes au sein de batîments appartenant à la commune.
+
+                </li>
+                <p></p>
+                <!--
+<hr>
+<li><strong>&Agrave; mes heures perdues... - Une Meilleure Vie Roleplay</strong>
+
+<ol><strong>Site internet</strong></ol>
+Participation à la création d'un UCP (Panel de contrôle Utilisateur) permettant à l'utilisateur de gérer ses données, son compte, ses personnages. (HTML - Javascript - CSS - SQL)<br>Le site est destiné à une communauté d'environ 100 joueurs. Il sert de complément à un serveur basé sur la plateforme <i>Multi Theft Auto : San Andreas</i>.
+
+<ol><strong>Quelques captures d'écran</strong></ol>
+<a href="assets/images/apercu_umv_1.png" target="_blank">Aperçu - Accueil</a> &bull; <a href="assets/images/apercu_umv_2.png" target="_blank">Aperçu - Boutique et dons</a> &bull; <a href="assets/images/apercu_umv_3.png" target="_blank">Aperçu - Équipe</a>
+
+<ol><strong>Création de visuels</strong></ol>
+<center><img src="assets/images/logo_umv.png" style="height: 160px;" alt="Logo d'UMV" title="Logo d'UMV"><img src="assets/images/logo_umv_noel.png" style="height: 160px;" alt="Logo d'UMV Fêtes" title="Logo d'UMV Fêtes"></center>
+
+</li>
+<p></p>
+</ul>
+-->
+
+                <!-- Compétences -->
+                <h2><img src="assets/images/competences.png" style="height:25px;" alt="Mes compétences" title="Mes compétences"> &bull; Mes compétences</h2>
+
+                <ul>
+                    <li>
+                        <strong>Adobe Photoshop</strong>
+                        <ol>
+                            <img src="assets/images/pt_competences2.png" style=" height: 20px;"><img src="assets/images/pt_competences2.png" style=" height: 20px;"> - Bases
+                        </ol>
+                    </li>
+                    <li>
+                        <strong>Photofiltre</strong>
+                        <ol>
+                            <img src="assets/images/pt_competences2.png" style=" height: 20px;"><img src="assets/images/pt_competences2.png" style=" height: 20px;"><img src="assets/images/pt_competences2.png" style=" height: 20px;"> - Très bonnes notions
+                        </ol>
+                    </li>
+
+                    <li>
+                        <strong>HTML5</strong>
+                        <ol>
+                            <img src="assets/images/pt_competences.png" style=" height: 20px;"><img src="assets/images/pt_competences.png" style=" height: 20px;"><img src="assets/images/pt_competences.png" style=" height: 20px;"> - Très bonnes notions
+                        </ol>
+                    </li>
+                    <li>
+                        <strong>CSS</strong>
+                        <ol>
+                            <img src="assets/images/pt_competences.png" style=" height: 20px;"><img src="assets/images/pt_competences.png" style=" height: 20px;"><img src="assets/images/pt_competences.png" style=" height: 20px;"> - Très bonnes notions
+                        </ol>
+                    </li>
+                    <li>
+                        <strong>PHP</strong>
+                        <ol>
+                            <img src="assets/images/pt_competences.png" style=" height: 20px;"><img src="assets/images/pt_competences.png" style=" height: 20px;"><img src="assets/images/pt_competences.png" style=" height: 20px;"> - Très bonnes notions
+                        </ol>
+                    </li>
+                    <li>
+                        <strong>Javascript</strong>
+                        <ol>
+                            <img src="assets/images/pt_competences.png" style=" height: 20px;"> - Notions
+                        </ol>
+                    </li>
+                    <li>
+                        <strong>SQL (Requêtes, PhpMyAdmin, SQL Server)</strong>
+                        <ol>
+                            <img src="assets/images/pt_competences.png" style=" height: 20px;"><img src="assets/images/pt_competences.png" style=" height: 20px;"> - Bases
+                        </ol>
+                    </li>
+                    <li>
+                        <strong>Microsoft Office (Word, Excel, PowerPoint, OneNote)</strong>
+                        <ol>
+                            <img src="assets/images/pt_competences3.png" style=" height: 20px;"><img src="assets/images/pt_competences3.png" style=" height: 20px;"><img src="assets/images/pt_competences3.png" style=" height: 20px;"> - Très bonnes notions
+                        </ol>
+                    </li>
+
+
+
+                </ul>
+
+                <!-- Mes sites favoris -->
+                <h2><img src="assets/images/sites.png" style="height:25px;" alt="Mes sites favoris" title="Mes sites favoris"> &bull; Mes sites favoris</h2>
+
+                <center>
+
+                    <!-- OpenClassrooms -->
+                    <img src="assets/images/openclassrooms.png" style="height:60px" alt="Logo d'Openclassrooms" title="Logo d'Openclassrooms">
+                    <h4>OpenClassrooms</h4>
+                    <p>C'est l'un des premiers sites sur lequel j'ai appris le développement. À l'époque nommé &laquo; Le site du zéro &raquo;, celui-ci m'a permis d'approfondir mes connaissances et de poser des questions aux plus expérimentés afin de mieux comprendre le développement web.</p>
+
+                    <hr />
+
+                    <!-- Facebook -->
+                    <img src="assets/images/facebook.png" style="height:30px" alt="Logo de Facebook" title="Logo de Facebook">
+                    <h4>Facebook</h4>
+                    <p>Facebook est un réseau social de partage et de rencontres. Celui-ci permet de connaître les actualités, les dernières nouveautés ou même encore de recevoir de l'aide dans un domaine en particulier. Il peut s'avérer très utile.</p>
+
+                    <hr />
+
+                    <!-- YouTube -->
+                    <img src="assets/images/youtube.svg" style="height:30px" alt="Logo de Youtube" title="Logo de Youtube">
+                    <h4>Youtube</h4>
+                    <p>YouTube me permet de consulter une multitudes de vidéos, de tutoriels et de vidéos explicatives sur le développement web. Avec les explications proposés par diverses personnes, cela facilite l'apprentissage et la compréhension.</p>
+
+                </center>
+
+                <!-- Mes centres d'intérêts -->
+                <h2><img src="assets/images/centres.png" style="height:20px;" alt="Mes centres d'intérêts" title="Mes centres d'intérêts"> &bull; Mes centres d'intérêts</h2>
+
+                <h3>Le vélo... <img src="assets/images/velo.png" style="height:25px;"></h3>
+                <p>J'aime depuis tout petit faire du vélo pour mes loisirs. Cela me permet de prendre l'air, de faire de drôles de rencontres (c.f photos) et de se vider l'esprit.</p>
+                <a href="assets/images/velo_maxime1.jpg" target="_blank"><img src="assets/images/velo_maxime.png" style="height:95px;"></a>
+                <a href="assets/images/velo_maxime3.jpg" target="_blank"><img src="assets/images/velo_maxime3p.png" style="height:95px;"></a>
+                <a href="assets/images/velo_maxime4.jpg" target="_blank"><img src="assets/images/velo_maxime4.png" style="height:95px;"></a>
+
+                <h3>Voyager <img src="assets/images/voyager.png" style="height:25px;"></h3>
+                <p>J'apprécie également voyager. Découvrir de nouvelles cultures, de nouvelles gastronomies et de nouveaux pays. J'ai eu l'opportunité de voyager dans déjà une dizaine de pays.</p>
+                <i>Portugal, France, Danemark, Pays-Bas, Suède, Belgique, Italie, Principauté de Monaco, Espagne, Allemagne...</i>
+                <p></p>
+
+                <a href="assets/images/portugal1.jpg" target="_blank"><img src="assets/images/portugal1.png" style="height:95px;"></a>
+                <a href="assets/images/portugal2.jpg" target="_blank"><img src="assets/images/portugal2.png" style="height:95px;"></a>
+                <a href="assets/images/italie.jpg" target="_blank"><img src="assets/images/italiep.png" style="height:95px;"></a>
+                <a href="assets/images/amsterdam.jpg" target="_blank"><img src="assets/images/amsterdamp.png" style="height:95px;"></a>
+                <a href="assets/images/pena.jpg" target="_blank"><img src="assets/images/penap.png" style="height:95px;"></a>
+
+                <h3>Informatique et développement <img src="assets/images/informatique.png" style="height:25px;"></h3>
+                <p>Comme vous avez pu vous en douter, je suis passionné d'informatique et de développement. Cela occupe donc une bonne partie de mon temps. </p>
+                <a href="assets/images/siteduzero.jpg" target="_blank"><img src="assets/images/siteduzero_livre.png" style="height:95px;"></a>
+                <a href="assets/images/mysqlnuls.jpg" target="_blank"><img src="assets/images/nuls.png" style="height:95px;"></a>
+                <a href="assets/images/joomla.jpg" target="_blank"><img src="assets/images/joomla.png" style="height:95px;"></a>
+
+                <!-- Mes réseaux sociaux -->
+                <h2><img src="assets/images/social.png" style="height:20px;" alt="Mes réseaux sociaux" title="Mes réseaux sociaux"> &bull; Mes réseaux sociaux</h2>
+
+                <center><a href="https://www.facebook.com/maximemendes37000" target="_blank"><img style="height:95px;" src="assets/images/facebook_maxime.png"></a></center>
+
+                <center><a href="https://www.linkedin.com/in/maxime-mendes-241762155/" target="_blank"><img style="height:95px;" src="assets/images/linkedin.png"></a></center>
+
+                <!-- Me contacter -->
+                <h2><img src="assets/images/contact.png" style="height:20px;" alt="Me contacter" title="Me contacter"> &bull; Me contacter</h2>
+
+                <center>
+                    <img src="assets/images/telephone.png" style="height:60px;">
+                    <h3>Par téléphone : 06 19 57 53 26</h3>
+
+                    <img src="assets/images/mail.png" style="height:60px;">
+                    <h3>Email : <a href="mailto:m95.md@outlook.fr" target="_blank">m95.md@outlook.fr</a><br>Email : <a href="mailto:maxime@mendes.re" target="_blank">maxime@mendes.re</a></h3>
+
+
+                </center>
+
+                </article>
+            <!-- Plugin Facebook - Vivre avec Leur Diabète -->
+            <div id="fb-root"></div>
+            <script>(function(d, s, id) {
+                    var js, fjs = d.getElementsByTagName(s)[0];
+                    if (d.getElementById(id)) return;
+                    js = d.createElement(s); js.id = id;
+                    js.src = 'https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v3.2';
+                    fjs.parentNode.insertBefore(js, fjs);
+                }(document, 'script', 'facebook-jssdk'));</script>
+            <!-- Plugin Facebook - Vivre avec Leur Diabète -->
+            </body>
+        </html>
